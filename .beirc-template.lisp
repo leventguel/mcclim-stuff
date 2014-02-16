661ce9b03a36f4e13fd6964c0f81ff7a44434515
leventguel <levent.guel@googlemail.com>
2014-02-16 20:12:29 +0100

    an ~/.beirc.lisp template for newbies!

---
+++ .beirc-template.lisp
@@ -0,0 +1,10 @@
+(setf *default-fill-column* 110)
+(setf *default-sound-player* nil)
+(setf *default-nick* (format nil "wbooze"))
+(setf *default-realname* (format nil "homie"))
+(setf *default-web-browser* "/usr/bin/firefox")
+(setf *auto-connect-list* '("irc.freenode.net"))
+(setf *auto-identify-list* '("irc.freenode.net"))
+(setf *nickserv-password-alist* '(("irc.freenode.net" . "password")))
+
+(setf *auto-join-alist* '(("irc.freeenode.net" . ("#maxima" "#lisp" "#clnoobs" "#sbcl" "#lispgames" "#scheme" "#emacs" "#algorithms" "##cs" "##programming" "##math" "##asm" "##linux" "##kernel" "##slackware" "#freenode"))))
